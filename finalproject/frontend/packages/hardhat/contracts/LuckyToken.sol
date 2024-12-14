eb8555d3a4f8ebcc66608eda10bac255e9a21965
Kate H. <106677924+kjhashim@users.noreply.github.com>
2024-12-14 23:31:04 +0000

    Update LuckyToken.sol

--- finalproject/frontend/packages/hardhat/contracts/LuckyToken.sol
+++ finalproject/frontend/packages/hardhat/contracts/LuckyToken.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract LuckyToken is ERC20, ERC20Permit, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("LuckyToken", "LTK") ERC20Permit("LuckyToken") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+}
