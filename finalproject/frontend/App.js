53f5dd4a77e4fbeb5c15b9d6a10dce075de96867
Rat100-ui <90065158+Rat100-ui@users.noreply.github.com>
2024-12-15 10:19:24 -0700

    Add files via upload

---
+++ finalproject/frontend/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import StashActions from './StashActions';
+
+function App() {
+  return (
+    <div className="App">
+      <StashActions />  {/* Render the StashActions component */}
+    </div>
+  );
+}
+
+export default App;  
+
+
+
+
+
+
+
