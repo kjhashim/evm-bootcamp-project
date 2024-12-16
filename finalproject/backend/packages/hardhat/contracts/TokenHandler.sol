bfd04d6b64341064cf543b046127d916622f9e61
Kate H. <106677924+kjhashim@users.noreply.github.com>
2024-12-16 22:14:48 +0000

    Add files via upload

---
+++ finalproject/backend/packages/hardhat/contracts/TokenHandler.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./LuckyToken.sol"; // Import the ERC-20 LuckyToken interface
+
+/// @title TokenHandler contract to track user (stashes) and game (kitty) accounts
+/// @author Groups 1, 6
+/// @dev This contract interacts with the LuckyToken transfer tokens between the token contract and stashes and the kitty. Each game has its own TokenHandler
+contract TokenHandler {
+    LuckyToken public token; // ERC-20 token address
+    address public ownerAddress;
+    address public masterAccountAddress;
+    // stashBalances + kittyBalance should equal 
+    // Track stash balances for each user address
+    mapping(address => uint256) public stashBalances;
+    // Track this game's kitty balance
+    uint256 private kittyBalance;
+
+    // token address: 
+    constructor(address _tokenAddress) {
+        ownerAddress = msg.sender;
+        token = LuckyToken(_tokenAddress);
+        masterAccountAddress = _tokenAddress;
+        kittyBalance = 0;
+    }
+
+    // Modifier to restrict access to the owner
+    modifier onlyOwner() {
+        require(msg.sender == ownerAddress, "Not the owner. Only owner may do this.");
+        _;
+    }
+
+    // Move tokens from user's wallet to user's stash address(this)
+    function depositWalletToStash(uint256 _numTokens) external {
+        require(token.transferFrom(msg.sender, address(this), _numTokens), 
+            string.concat(
+                "Failed to deposit ", Strings.toString(_numTokens),
+                "tokens from wallet to stash"
+            )
+        );
+        stashBalances[msg.sender] += _numTokens;
+    }
+
+    // withdraw tokens from user's stash to user's wallet
+    function withdrawStashToWallet(uint256 _numTokens) external onlyOwner {
+        require(stashBalances[msg.sender] >= _numTokens,
+            string.concat(
+                "Insufficient token balance ", Strings.toString(stashBalances[msg.sender]),
+                "in user stash to withdraw", Strings.toString(_numTokens)
+            )
+        );
+        stashBalances[msg.sender] -= _numTokens;
+        require(token.transfer(msg.sender, _numTokens),
+            string.concat(
+                "Withdrawal to wallet of ", Strings.toString(_numTokens),
+                "tokens failed"
+            )
+        );
+    }
+
+    // Move tokens from user's stash to game kitty
+    function depositStashToGameKitty(uint256 _numTokens) external {
+        require(stashBalances[msg.sender] >= _numTokens, "Insufficient balance in user stash");
+        stashBalances[msg.sender] -= _numTokens;
+        kittyBalance += _numTokens;
+    }
+
+    // Move tokens from game kitty to user's stash
+    function withdrawGameKittyToStash(uint256 _numTokens) external {
+        require(kittyBalance >= _numTokens, "Insufficient balance in game kitty to send to user stash");
+        kittyBalance -= _numTokens;
+        stashBalances[msg.sender] += _numTokens;
+    }
+
+    // transfer tokens from game kitty to Master Account
+    function transferToMasterAccount(uint256 _numTokens) external {
+        require(token.transferIn(address(this), _numTokens), "Transfer from kitty to master account failed");
+    }
+
+    // transfer tokens from Master Account to game kitty
+    function transferFromMasterAccount(uint256 _numTokens) external {
+        require(token.transferOut(address(this), _numTokens), "Transfer from master account to kitty failed");
+    }
+
+    // Function for current user to send tokens to another address
+    function sendTokens(address recipient, uint256 _numTokens) external {
+        require(stashBalances[msg.sender] >= _numTokens, "Insufficient balance");
+        require(token.transfer(recipient, _numTokens), "Transfer failed");
+        stashBalances[msg.sender] -= _numTokens;
+    }
+
+    // Function to check this game's total balance (should be stashBalances + kittyBalance)
+    function getGameBalance() external view returns (uint256) {
+        return token.balanceOf(address(this));
+    }
+
+    // Function to check an individual address's token balance in the contract
+    function getStashBalance(address _userAddress) external view returns (uint256) {
+        require((msg.sender == _userAddress) || (msg.sender == ownerAddress), "Only the owner or the House can check stash balance");
+        return stashBalances[_userAddress];
+    }
+
+    // Function to get this game's kitty balance
+    function getGameKittyBalance() external view returns (uint256) {
+        return kittyBalance;
+    }
+
+}
---
+++ finalproject/backend/packages/hardhat/contracts/Wheel.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "./TokenHandler.sol";
+
+/// @title Spin the Wheel contract
+/// @author Groups 1, 6
+contract Wheel is Ownable {
+    uint public minBet = 1;
+    uint public maxBet = 10000;
+    uint public houseSkewPercentage = 1;
+    TokenHandler public tokenHandler; // track user balances, transfer tokens
+    /// spin the wheel choices
+    string[] spinChoices = ["red", "white"];
+
+    /// @notice Flag indicating whether the lottery is open for bets or not
+    bool private gameOpen;
+
+    /// @notice Constructor function
+    /// @param _tokenHandlerAddress address of TokenHandler for this game
+    constructor(address _tokenHandlerAddress) Ownable(msg.sender) {
+        tokenHandler = TokenHandler(_tokenHandlerAddress);
+        gameOpen = true;
+    }
+
+    function openGame() external onlyOwner {
+        gameOpen = true;
+    }
+
+    function closeGame() external onlyOwner {
+        gameOpen = false;
+    }
+    function isGameOpen() public view returns (bool) {
+        return gameOpen;
+    }
+
+    function bet(uint _numTokensBet, string memory _colorBet) external {
+        require(gameOpen, "Game is closed");
+        require((_numTokensBet >= minBet) && (_numTokensBet <= maxBet),
+            string.concat(
+                "Invalid number of tokens bet. You must bet at least",
+                Strings.toString(minBet),
+                "tokens and no more than ",
+                Strings.toString(maxBet)
+            )
+        );
+        require(existsElement(_colorBet, spinChoices), "Invalid color bet");
+        // check that user has enough balance to cover bet
+        uint256 userBalance = tokenHandler.getStashBalance(msg.sender);
+        require(userBalance >= _numTokensBet,
+            string.concat(
+                "User stash balance of ", Strings.toString(userBalance),
+                "is insufficient to cover bet of ",
+                Strings.toString(_numTokensBet),
+                ". Please add more tokens to your stash."
+            )
+        );
+        // check that kitty has enough balance to cover bet
+        uint256 kittyBalance = tokenHandler.getGameKittyBalance();
+        require(kittyBalance >= _numTokensBet,
+            string.concat(
+                "Game kitty balance of ", Strings.toString(kittyBalance),
+                "is insufficient to cover bet of ",
+                Strings.toString(_numTokensBet)
+            )
+        );
+        string memory outcomeColor = chooseOutcome(_colorBet);
+        // bet won, transfer from kitty to user stash
+        if (keccak256(abi.encodePacked(_colorBet)) == keccak256(abi.encodePacked(outcomeColor))) {
+            tokenHandler.withdrawGameKittyToStash(_numTokensBet);
+        }
+        else { // bet lost
+            tokenHandler.depositStashToGameKitty(_numTokensBet);
+        }
+
+    }
+
+    // Function to choose an outcome based on skewed probability
+    function chooseOutcome(string memory _chosenBet) public view returns (string memory) {
+        // probability of each outcome if each equally likely
+        uint256 defaultProb = 100/spinChoices.length;
+        // adjust probabilities by house skew
+        uint256 chosenProb = defaultProb - houseSkewPercentage;
+        uint256 otherProb = defaultProb - (houseSkewPercentage/(spinChoices.length-1));
+        // Get a random number between 1 and 100
+        uint256 randomValue = uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao))) % 100;
+        
+        uint256 cumulativeWeight = 0;
+
+        // Loop through the outcomes and pick the one based on the random number
+        for (uint256 i = 0; i < spinChoices.length; i++) {
+            if (keccak256(abi.encodePacked(_chosenBet)) == keccak256(abi.encodePacked(spinChoices[i]))) {
+                cumulativeWeight += chosenProb;
+            }
+            else {
+                cumulativeWeight += otherProb;
+            }
+            if (randomValue < cumulativeWeight) {
+                // The outcome has been chosen
+                return spinChoices[i];
+            }
+        }
+        return "";
+    }
+
+
+    /// check if element exists in the array
+    function existsElement(string memory _element, string[] memory _array) public pure returns (bool) {
+        for (uint i = 0; i < _array.length; i++) {
+            if (keccak256(abi.encodePacked(_element)) == keccak256(abi.encodePacked(_array[i]))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
